6c5de565f0ecc414aa0b7efb58e226cb2428fb2e
Lauri Seppäläinen <lauri.seppalainen@helsinki.fi>
2025-01-30 11:23:22 +0200

    add paper theme

--- experiment/utils/utils.py
+++ experiment/utils/utils.py
@@ -24,6 +24,7 @@ from functools import reduce
 from experiment.utils.hyperparameters import get_bb, get_data, get_params
 import matplotlib.pyplot as plt
 import seaborn as sns
+from typing import Union, Dict, Tuple
 
 
 OUTPUT_DIR = RESULTS_DIR / "k_sensitivity"
@@ -625,3 +626,49 @@ def plot_pyramid():
     ax[0].set_ylabel("f(x)")
     ax[1].set_xlabel("x")
     # plt.savefig(MANUSCRIPT_DIR / "pyramid_example.pdf", dpi=600)
+
+
+def paper_theme(
+    width: float = 1.0,
+    aspect: float = 1.0,
+    cols: int = 1,
+    rows: int = 1,
+    page_width: float = 347.0,
+    figsize: bool = False,  # return figsize instead of dict
+) -> Union[Dict[str, float], Tuple[float, float]]:
+    """Set theme and sizes for plots added to papers.
+
+    Usage:
+        sns.relplot(..., **paper_theme(...))
+        plt.subplots(..., figsize=paper_theme(..., figsize=True))
+
+    Args:
+        width: Fraction of page width. Defaults to 1.0.
+        aspect: Aspect ratio of plots. Defaults to 1.0.
+        cols: Number of columns in plot. Defaults to 1.
+        rows: Number of rows in plot. Defaults to 1.
+        page_width: Page width in points. Defaults to 347.0.
+        figsize: Return figsize instead of dict (for use with bare pyplot figures). Defaults to False.
+
+    Returns:
+        The size.
+    """
+    if width == 1.0:
+        width = 0.99
+    scale = page_width / 72.27  # from points to inches
+    size = (width * scale, width / cols * rows / aspect * scale)
+    sns.set_theme(
+        context={k: v * 0.6 for k, v in sns.plotting_context("paper").items()},
+        style=sns.axes_style("ticks"),
+        palette="bright",
+        # font="cmr10",
+        rc={
+            "figure.figsize": size,
+            "savefig.bbox": "tight",
+            "savefig.pad_inches": 1e-4,
+        },
+    )
+    if figsize:
+        return size
+    else:
+        return dict(height=width * scale / cols * aspect, aspect=aspect)
