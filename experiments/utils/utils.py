ea7e85329b3e553b4d2cc32893d45287895dc898
Lauri Seppäläinen <lauri.seppalainen@helsinki.fi>
2025-10-22 12:10:16 +0300

    start rewrite; add config.ini

--- experiments/utils/utils.py
+++ experiments/utils/utils.py
@@ -57,12 +57,12 @@ def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def prepare_data(dataset_fun, seed):
+def prepare_data(dataset_fun, seed, n_total=10_000, n_exp=500):
 
     X, y, bb, cls = dataset_fun()
 
     # Limit the data size to a maximum of 10,000 samples
-    n = min(10_000, X.shape[0])
+    n = min(n_total, X.shape[0])
     X, y = X[:n, :], y[:n]
 
     # Ensure y has at least 2 dimensions
@@ -70,14 +70,26 @@ def prepare_data(dataset_fun, seed):
         y = y[:, None]
 
     # Split the data into training and testing sets, with a 50/50 split
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.5, random_state=seed
-    )
-
-    # Further split the training to get a smaller training subset for experiments
-    X_exp_train, _, y_exp_train, _ = train_test_split(
-        X_train, y_train, train_size=500, random_state=seed
-    )
+    if cls:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.5, random_state=42, stratify=y[:, 1]
+        )
+        # Further split the training to get a smaller training subset for experiments
+        X_exp_train, _, y_exp_train, _ = train_test_split(
+            X_train,
+            y_train,
+            train_size=n_exp,
+            random_state=seed,
+            stratify=y_train[:, 1],
+        )
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.5, random_state=seed
+        )
+        # Further split the training to get a smaller training subset for experiments
+        X_exp_train, _, y_exp_train, _ = train_test_split(
+            X_train, y_train, train_size=n_exp, random_state=seed
+        )
 
     # Convert all data arrays into PyTorch tensors
     X_train = torch.as_tensor(X_train, dtype=torch.float32)
@@ -122,7 +134,20 @@ def get_explainer(dname, bb, cls):
     }
 
 
-def get_optimisation_method(k: int):
+def get_explanation_radius(explainer: lm.Explainer):
+    if "LIMEExplainer" in str(explainer.__class__):
+        return 2 * explainer.explainer_kwargs["kernel_width"]
+    elif "SLISEMAP" in str(explainer.__class__) or "SLIPMAP" in str(
+        explainer.__class__
+    ):
+        return 0.1 * explainer.explainer_kwargs["radius"]
+    elif "Smoothgrad" in str(explainer.__class__):
+        return explainer.explainer_kwargs["noise_level"]
+    else:
+        return 0.5
+
+
+def get_optimisation_method(k: int, explainer: lm.Explainer):
     """Define the reducing methods to be used in the experiments."""
     return {
         (f"greedy_max_coverage_k_{k}", k): partial(px.find_proxies_greedy, k=k),
@@ -130,7 +155,6 @@ def get_optimisation_method(k: int):
             px.find_proxies_coverage, k=k, time_limit=100, pulp_msg=False
         ),
         (f"random_k_{k}", k): partial(px.find_proxies_random, k=k),
-        # (f"min_loss_{k}", k): partial(px.find_proxies_loss, k=k, time_limit=300),
         (f"greedy_min_loss_{k}", k): partial(px.find_proxies_greedy_k_min_loss, k=k),
         (f"greedy_min_loss_{k}_min_cov", k): partial(
             px.find_proxies_greedy_min_loss_k_min_cov,
@@ -159,6 +183,14 @@ def get_optimisation_method(k: int):
             method="kmeans",
             dist="euclidean",
         ),
+        (f"submodular_pick_{k}", k): partial(px.find_proxies_submodular_pick, k=k),
+        (f"max_ball_coverage_{k}", k): partial(
+            px.find_proxies_max_cov_ball_cover,
+            k=k,
+            radius=get_explanation_radius(explainer),
+            time_limit=5000,
+        ),
+        (f"glocalx_{k}", k): partial(px.find_proxies_glocalx, k=k),
     }
 
 
@@ -233,148 +265,6 @@ def eval_proxy_method_k_sensitivity(
     )
 
     return res
-
-
-def evaluate_k_sensitivity(job_id: int, ks: list[int]) -> None:
-    """Evaluate the full set of optimisation problems."""
-    print(f"Begin job {job_id}.", flush=True)
-
-    # Set random seeds for reproducibility
-    np.random.seed(1618 + job_id)
-    torch.manual_seed(1618 + job_id)
-
-    # Determine output file path and ensure its directory exists
-    output_file = OUTPUT_DIR / f"k_sensitivity_{job_id:02d}.parquet"
-    output_file.parent.mkdir(parents=True, exist_ok=True)
-
-    # Load the results if they exist, otherwise create an empty DataFrame
-    if output_file.exists():
-        results = read_parquet(output_file)
-    else:
-        results = pd.DataFrame()
-
-    # Iterate through the datasets and their respective preparation functions
-    for dname, ds_fn in get_data().items():
-        print(f"Preparing:   {dname}")
-
-        # Prepare the dataset and extract its components
-        ds = prepare_data(ds_fn, seed=1618 + job_id)
-        cls = ds["classifier"]
-        bb_name = ds["black_box"]
-        X_test, y_test = ds["test"]
-
-        # Define the loss function based on whether it is a classification task
-        loss_fn = (
-            torch.nn.MSELoss(reduction="none")
-            if not cls
-            else lm.logistic_regression_loss
-        )
-
-        # Retrieve the training data, the black-box model, and its prediction function
-        X_bb, y_bb = ds["bb_train"]
-        m, pred_fn = get_bb(bb_name, cls, X_bb, y_bb, dname)
-
-        # Make predictions on the test set and training set
-        yhat_test = pred_fn(X_test)
-        yhat_test = torch.as_tensor(yhat_test, dtype=y_bb.dtype)
-        if yhat_test.ndim < y_bb.ndim:
-            yhat_test = yhat_test[:, None]
-        yhat_train = torch.as_tensor(pred_fn(X_bb), dtype=y_bb.dtype)
-        if yhat_train.ndim < y_bb.ndim:
-            yhat_train = yhat_train[:, None]
-
-        # Compute the loss on the training data and determine the global epsilon
-        loss_train = loss_fn(yhat_train, y_bb)
-        global_epsilon = torch.quantile(loss_train, 0.2).item()
-
-        # Iterate through different explainer methods
-        for expname, expfn in get_explainer(dname, bb_name, cls).items():
-            X, y = ds["exp_train"]
-
-            # Compute pairwise distances and nearest neighbors
-            D = torch.cdist(torch.as_tensor(X), torch.as_tensor(X))
-            D += torch.eye(D.shape[0]) * torch.max(D)
-            nn = torch.argsort(D, 1)[:, :5]
-
-            # Fit the explainer to the training data
-            yhat = pred_fn(X)
-            explainer = expfn(X, yhat, black_box_model=m)
-            explainer.fit()
-
-            # Get the explanation matrix and make predictions on the test set
-            L = explainer.get_L()
-            explainer_yhat_test = explainer.predict(X_test)
-
-            # Compute fidelity and loss metrics
-            full_fidelity = (
-                loss_fn(
-                    torch.as_tensor(yhat_test),
-                    explainer_yhat_test,
-                )
-                .mean()
-                .item()
-            )
-            full_loss = (
-                loss_fn(
-                    torch.as_tensor(y_test),
-                    explainer_yhat_test,
-                )
-                .mean()
-                .item()
-            )
-            bb_loss = (
-                loss_fn(
-                    torch.as_tensor(y_test),
-                    yhat_test,
-                )
-                .mean()
-                .item()
-            )
-
-            # Loop over k values and their corresponding optimization methods
-            print("Loop over k values.")
-            k_options = [get_optimisation_method(k=k) for k in ks]
-            k_options = reduce(operator.ior, k_options, {})
-            for (pname, k), proxy_method in k_options.items():
-                # Skip already computed results
-                if (
-                    not results.empty
-                    and (
-                        (results["data"] == dname)
-                        & (results["exp_method"] == expname)
-                        & (results["proxy_method"] == pname)
-                        & (results["k"] == k)
-                    ).any()
-                ):
-                    continue
-                # Evaluate the proxy method and store results
-                res = eval_proxy_method_k_sensitivity(
-                    job_id,
-                    dname,
-                    expname,
-                    explainer,
-                    pname,
-                    proxy_method,
-                    k,
-                    global_epsilon,
-                    X,
-                    X_test,
-                    y,
-                    y_test,
-                    yhat_test,
-                    loss_fn,
-                    L,
-                    full_fidelity,
-                    full_loss,
-                    bb_loss,
-                    nn,
-                )
-                if res is not None:
-                    results = pd.concat(
-                        (results, pd.DataFrame([res])), ignore_index=True
-                    )
-                    results.to_parquet(output_file)
-    print("Done.", flush=True)
 
 
 def subsample_explainer(explainer: lm.Explainer, n: int) -> lm.Explainer:
